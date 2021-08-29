e2f0c8f484cf7976cf748974f1dab7454fb29e20
dharmil06 <dharmilp57@gmail.com>
2021-08-29 06:39:10 -0700

    Test hosting update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Create dice-cube app using react</h1>
+     <h1>Create dice-cube app using react v1</h1>
     </div>
   );
 }
