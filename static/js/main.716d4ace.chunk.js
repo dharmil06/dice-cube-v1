a8a71055097c8ab315a3c365cf21b5b12fb6614d
dharmil06 <dharmilp57@gmail.com>
2021-08-29 06:45:25 -0700

    Updates

--- static/js/main.716d4ace.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpdice-cube-v1"]=this["webpackJsonpdice-cube-v1"]||[]).push([[0],{11:function(e,c,t){"use strict";t.r(c);var n=t(1),i=t.n(n),r=t(3),s=t.n(r),u=(t(8),t(9),t(0));var a=function(){return Object(u.jsx)("div",{className:"App",children:Object(u.jsx)("h1",{children:"Create dice-cube app using react"})})};s.a.render(Object(u.jsx)(i.a.StrictMode,{children:Object(u.jsx)(a,{})}),document.getElementById("root"))},8:function(e,c,t){},9:function(e,c,t){}},[[11,1,2]]]);
-//# sourceMappingURL=main.716d4ace.chunk.js.map
